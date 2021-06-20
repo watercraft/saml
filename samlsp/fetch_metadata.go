f9ecadb1bed7ac38fbea24d6f237dec87b513ed5
Alan Jones <alan@zebrium.com>
2021-06-20 07:03:37 -0700

    convert import of httperr back to crewjam

--- samlsp/fetch_metadata.go
+++ samlsp/fetch_metadata.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 	"net/url"
 
-	"github.com/watercraft/httperr"
+	"github.com/crewjam/httperr"
 	xrv "github.com/mattermost/xml-roundtrip-validator"
 
 	"github.com/watercraft/saml"
